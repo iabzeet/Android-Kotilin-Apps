3d1a9bde8cac6af3ed1fc6b6ef74649631517f52
iabzeet <iabzeet@gmail.com>
2024-05-24 22:36:44 +0530

    RecyclerView added with adapter

---
+++ MyMaps/app/src/main/java/com/example/mymaps/MapsAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.mymaps
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymaps.models.UserMap
+
+private const val TAG = "MapsAdapter"
+class MapsAdapter(val context: Context, val userMaps: List<UserMap>) : RecyclerView.Adapter<MapsAdapter.ViewHolder>() {
+
+    //inflate the view and wrap that inside of a VH
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = userMaps.size
+
+    //grab the data at the position and bind it into a view located inside of a VH
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val userMap = userMaps[position]
+        //setting a clicklistener on itemview
+        holder.itemView.setOnClickListener {
+            Log.i(TAG, "Tapped on position $position")
+        }
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+        textViewTitle.text = userMap.title
+    }
+
+    //VH takes the contructor of the parent class which is itemview
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
---
+++ MyMaps/app/src/main/java/com/example/mymaps/models/Place.kt
@@ -0,0 +1,2 @@
+package com.example.mymaps.models
+data class Place(val title: String, val description: String, val latitude: Double, val longitude: Double)
