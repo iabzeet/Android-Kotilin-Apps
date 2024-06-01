a9188861d0780161d647d610b7d89cd06b27371b
iabzeet <iabzeet@gmail.com>
2024-06-01 23:57:32 +0530

    MainActivity.kt modified

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,14 +1,55 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.children
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //binding
+    private lateinit var binding: ActivityMainBinding
+
+    private var firstNumber = ""
+    private var currentNumber = ""
+    private var currentOperator = ""
+    private var result = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //initViews
+        binding.apply {
+            //get all buttons
+            binding.layoutMain.children.filterIsInstance<Button>().forEach { button ->
+                //buttons click listener
+                button.setOnClickListener {
+                    //get clicked button text
+                    val buttonText = button.text.toString()
+                    when {
+                        buttonText.matches(Regex("[0-9]")) -> {
+
+                        }
+                        buttonText.matches(Regex("[+\\-*/]")) -> {
+
+                        }
+                        buttonText == "=" -> {
+
+                        }
+                        buttonText == "." -> {
+
+                        }
+                    }
+                }
+            }
+
+        }
+
     }
 }
